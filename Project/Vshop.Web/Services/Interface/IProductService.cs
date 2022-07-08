40f5bf1a7f5f77c1adc9b66c05a100fb7777dfc5
MathsPrado <matheusprado55@gmail.com>
2022-07-08 20:08:58 -0300

    Add Services in Web

--- Project/Vshop.Web/Services/Interface/IProductService.cs
+++ Project/Vshop.Web/Services/Interface/IProductService.cs
@@ -5,5 +5,10 @@ namespace Vshop.Web.Services.Interface;
 public interface IProductService
 {
     Task<IEnumerable<ProductViewmodel>> GetAllProducts();
+    Task<ProductViewmodel> FindProductById(int id);
+    Task<ProductViewmodel> CreateProduct (ProductViewmodel productVM);
+    Task<ProductViewmodel> UpdateProduct (ProductViewmodel productVM);
+    Task<bool> DeleteProduct (int id);
 }
 
+  
---
+++ Project/Vshop.Web/Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Vshop.Web.Models.ViewModel;
+using Vshop.Web.Services.Interface;
+
+namespace Vshop.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private const string apiEndpoint = "api/products/";
+        private readonly JsonSerializerOptions _opt;
+        private ProductViewmodel productVm;
+        private IEnumerable<ProductViewmodel> productsVm;
+
+        public ProductService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _opt = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public Task<IEnumerable<ProductViewmodel>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductViewmodel> FindProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductViewmodel> CreateProduct(ProductViewmodel productVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductViewmodel> UpdateProduct(ProductViewmodel productVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
