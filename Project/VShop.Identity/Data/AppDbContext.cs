1144e5d9a6eb36c39229767134c1f426210c6574
MathsPrado <matheusprado55@gmail.com>
2022-07-25 21:43:29 -0300

    Added identity Server

---
+++ Project/VShop.Identity/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VShop.Identity.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
